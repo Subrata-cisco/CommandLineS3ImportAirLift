85c363c3b6d2f3899bc3fc7ce9bd604cb4ca5382
Subrata <saha.subrata@gmail.com>
2018-11-18 21:31:03 +0530

    corrected the script

--- src/main/resources/startimport.sh
+++ src/main/resources/startimport.sh
@@ -3,34 +3,36 @@
 
 echo "Script will attemp to get the files from linux machine..."
 
-username="8"
-password="@8"
-host="8.us.8.com"
-remoteDir="/home/8/dump/*.*"
-localDir="/scratch/8/subrata/hari/"
+username="uuname"
+password="ppname"
+host="elc.amazon.com"
+remoteDir=/home/supi/dump/*.*
+localDir="/scratch/supi/subrata/hari/"
 
-# TODO - Clean the local directory first..
-
-
-java -jar import-cli.jar import copy-from-unix -u $username -p $password -h $host -rd $remoteDir -ld $localDir
-if [ $? -eq 0 ]; then
-    echo Fetching files from $host passed.....
-else
-    echo Fetching files from $host failed.....
-	exit 1;
-fi
-
-echo "Script will attemp to put the files to S3 now.."
 aws_key="AKIAJME********W7B75A"
 aws_sec="DYE/s+HABC8l*************hdYAJWOWZwm"
 aws_bucket="bancsv"
 aws_bucket_region="us-west-2"
 
-java -jar import-cli.jar import process-file-command -k $aws_key -s $aws_sec -b $aws_bucket -r $aws_bucket_region -ld $localDir
+
+# TODO - Clean the local directory first..
+
+
+java -jar import-cli.jar import copy-from-unix -u "$username" -p "$password" -h "$host" -rd "$remoteDir" -ld "$localDir"
 if [ $? -eq 0 ]; then
-    echo Storing file to S3 from $localDir passed..
+    echo Fetching files from "$host" passed.....
 else
-    echo Storing file to S3 from $localDir failed..
+    echo Fetching files from "$host" failed.....
+	exit 1;
+fi
+
+echo "Script will attemp to put the files to S3 now.."
+
+java -jar import-cli.jar import process-file-command -k "$aws_key" -s "$aws_sec" -b "$aws_bucket" -r "$aws_bucket_region" -ld "$localDir"
+if [ $? -eq 0 ]; then
+    echo Storing file to S3 from "$localDir" passed..
+else
+    echo Storing file to S3 from "$localDir" failed..
 fi
 
 echo "All done !!"
